a681ab50c715a214b18d1a02c1dc692defd776e1
thiago.gigante <thiagoandraderego@gmail.com>
2023-12-01 23:12:28 -0300

    ajustes no link do botões e limpesa de código

--- script.js
+++ script.js
@@ -3,8 +3,3 @@ bot_enviar.addEventListener('click' , function(){
   alert("Acesso Concedido, Seja bem Vindo!")
   open('painel_agenda.html')
 } )
-
-const bot_agendar = document.querySelector('.agendaratendimento')
-bot_agendar.addEventListener('click', function () {
-  alert("Atendimento agendado com Sucesso!")
-  })
